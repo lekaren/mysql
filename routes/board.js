725327b398ae462402547bc9f5cdebfaf5ffbcfb
lekaren <cpdnql@naver.com>
2019-07-08 09:50:05 +0900

    게시글 수정, 댓글 기능 추가

--- routes/board.js
+++ routes/board.js
@@ -1,23 +1,16 @@
 
 var express = require('express');  
 var router = express.Router();
-const mysql = require('mysql');
 const bodyParser = require('body-parser');
-const pool = mysql.createPool({
-  connectionLimit : 10, 
-  host            : 'localhost',
-  user            : 'root',
-  password        : '1234',
-  database        : 'karen',
-  dateStrings     : 'date' 
-});
+const pool = require('../config/dbconfig');
 
 // 게시판으로 이동
 router.get('/board', function(req, res, next) {
 
   pool.getConnection(function(err, conn){
-    conn.query('SELECT * FROM board;', function(err, results){
-      res.render('board', { results: results });
+    conn.query(`SELECT a.*,(SELECT COUNT(*) FROM comment WHERE board_id=a.number) AS 'cc' FROM board AS a;`, function(err, results){
+      res.render('board/board', { results: results });
+      console.log(results.cc);
       
       conn.release();
     });
@@ -26,10 +19,9 @@ router.get('/board', function(req, res, next) {
 
 // 게시판글 등록으로 이동
 router.get('/create', function(req, res, next) {
-
   pool.getConnection(function(err, conn){
     conn.query(`SELECT * FROM board WHERE NAME='${req.session.ID}';`, function(err, results){
-      res.render('create', { results: results });
+      res.render('board/create', { results: results });
       
       conn.release();
     });
@@ -44,32 +36,107 @@ router.post('/create2', function(req, res, next) {
 
   pool.getConnection(function(err, conn){
     conn.query(`INSERT INTO board (title, content, id) VALUES ('${title}','${content}','${req.session.ID}');`, function(err, results){
-      res.redirect('/board/board');
+      res.redirect('board');
       
       conn.release();
     });
   });
 });
 
-/*게시판글 삭제*/
+// 게시판글 삭제 페이지
 router.get('/delete', function(req, res, next) {
 
   pool.getConnection(function(err, conn){
     conn.query(`SELECT * FROM board WHERE id='${req.session.ID}';`, function(err, results){
-      res.render('delete', { results: results });
+      res.render('board/delete', { results: results });
       
       conn.release();
     });
   });
 });
 
-/*게시판글 삭제*/
+// 게시판글 삭제
 router.get('/final', function(req, res, next) {
   const title = req.query.title;
   pool.getConnection(function(err, conn){
-    console.log(req);
     conn.query(`DELETE FROM board WHERE number='${req.query.number}'`, function(err, results){
-      res.redirect('/board/board');
+      res.redirect('board');
+      
+      conn.release();
+    });
+  });
+});
+
+// 작성한 글 페이지로 이동
+router.get('/cn', function(req, res, next) {
+  const board_id = req.query.number;
+  pool.getConnection(function(err, conn){
+    conn.query(`SELECT * FROM board WHERE number='${req.query.number}';`, function(err, results){
+      conn.query(`SELECT * FROM comment WHERE board_id='${req.query.number}';`, function(err, com){
+        res.render('board/content', {board_id:board_id , results : results , com:com});
+      });
+      
+      
+      conn.release();
+    });
+  });
+});
+
+// 글 수정 페이지
+router.get('/ud', function(req, res, next) {
+  const title = req.query.title;
+  const content = req.query.content;
+
+  pool.getConnection(function(err, conn){
+    conn.query(`SELECT * FROM board WHERE number='${req.query.number}';`, function(err, results){
+      res.render('board/update',{ result : results });
+      
+      conn.release();
+    });
+  });
+});
+
+// 글 수정 처리
+router.post('/update', function(req, res, next) {
+  const reqnumber = req.body.reqnumber;
+
+  pool.getConnection(function(err, conn){
+    conn.query(`UPDATE board SET title='${req.body.title}', content='${req.body.content}' WHERE number='${reqnumber}';`, function(err, results){
+      res.redirect('board');
+      
+      conn.release();
+    });
+  });
+});
+
+// 댓글 달기
+router.post('/cm', function(req, res, next) {
+  const reqnumber = req.body.reqnumber;
+  const number = req.query.number;
+  const comment = req.body.comment;
+  const board_id = req.body.board_id;
+
+  pool.getConnection(function(err, conn){
+    conn.query(`INSERT INTO comment(board_id, email, content) values('${board_id}', '${req.session.ID}', '${comment}');`, function(err, results){
+      conn.query(`SELECT * FROM board WHERE number='${req.query.number}';`, function(err, results){
+        // res.render('board/content', { results : results });
+        res.redirect(`/board/content?number=${comment}&email=${req.session.ID}&comment=${comment}`);
+      
+        conn.release();
+        });
+      });      
+  });
+});
+
+// 댓글 등록처리
+router.post('/comment', function(req, res, next) {
+  const reqnumber = req.body.reqnumber;
+  const board_id = req.body.board_id;
+  const comment = req.body.comment;
+
+  pool.getConnection(function(err, conn){
+    conn.query(`INSERT INTO comment(board_id, email, content) values('${board_id}', '${req.session.ID}', '${comment}');`, function(err, results){
+      res.redirect(`/board/cn?number=${board_id}`);
       
       conn.release();
     });
